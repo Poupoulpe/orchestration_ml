5068397d0fbfafae1adf5cbe62fa4bbb34c6d531
Daoud-Handas <daoudhandas@gmail.com>
2024-06-27 15:11:48 +0200

    Add daily data ingestion

--- data_ingestion.py
+++ data_ingestion.py
@@ -10,8 +10,8 @@ retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
 openmeteo = openmeteo_requests.Client(session=retry_session)
 
 # Calculate the start_date and end_date for "yesterday"
-yesterday = datetime.now() - timedelta(days=1)
-start_date = yesterday.strftime('%Y-%m-%d')
+two_day_ago = datetime.now() - timedelta(days=2)
+start_date = two_day_ago.strftime('%Y-%m-%d')
 end_date = start_date
 
 # Make sure all required weather variables are listed here
@@ -33,13 +33,86 @@ params = {
 # Make the API request
 response = openmeteo.weather_api(url=url, params=params)
 
-# Check if the request was successful
-if response.status_code == 200:
-    data = response.json()
-    # Process the data and save it to a CSV file
-    df = pd.DataFrame(data['hourly'])
-    csv_file = 'weather_data.csv'
-    df.to_csv(csv_file, index=False)
-    print(f"Weather data has been saved to {csv_file}")
-else:
-    print(f"Failed to fetch weather data: {response.status_code} - {response.text}")
+# Data preparation
+response = response[0]
+hourly = response.Hourly()
+hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
+hourly_relative_humidity_2m = hourly.Variables(1).ValuesAsNumpy()
+hourly_dew_point_2m = hourly.Variables(2).ValuesAsNumpy()
+hourly_apparent_temperature = hourly.Variables(3).ValuesAsNumpy()
+hourly_precipitation = hourly.Variables(4).ValuesAsNumpy()
+hourly_rain = hourly.Variables(5).ValuesAsNumpy()
+hourly_snowfall = hourly.Variables(6).ValuesAsNumpy()
+hourly_snow_depth = hourly.Variables(7).ValuesAsNumpy()
+hourly_weather_code = hourly.Variables(8).ValuesAsNumpy()
+hourly_pressure_msl = hourly.Variables(9).ValuesAsNumpy()
+hourly_surface_pressure = hourly.Variables(10).ValuesAsNumpy()
+hourly_cloud_cover = hourly.Variables(11).ValuesAsNumpy()
+hourly_cloud_cover_low = hourly.Variables(12).ValuesAsNumpy()
+hourly_cloud_cover_mid = hourly.Variables(13).ValuesAsNumpy()
+hourly_cloud_cover_high = hourly.Variables(14).ValuesAsNumpy()
+hourly_et0_fao_evapotranspiration = hourly.Variables(15).ValuesAsNumpy()
+hourly_vapour_pressure_deficit = hourly.Variables(16).ValuesAsNumpy()
+hourly_wind_speed_10m = hourly.Variables(17).ValuesAsNumpy()
+hourly_wind_speed_100m = hourly.Variables(18).ValuesAsNumpy()
+hourly_wind_direction_10m = hourly.Variables(19).ValuesAsNumpy()
+hourly_wind_direction_100m = hourly.Variables(20).ValuesAsNumpy()
+hourly_wind_gusts_10m = hourly.Variables(21).ValuesAsNumpy()
+hourly_soil_temperature_0_to_7cm = hourly.Variables(22).ValuesAsNumpy()
+hourly_soil_temperature_7_to_28cm = hourly.Variables(23).ValuesAsNumpy()
+hourly_soil_temperature_28_to_100cm = hourly.Variables(24).ValuesAsNumpy()
+hourly_soil_temperature_100_to_255cm = hourly.Variables(25).ValuesAsNumpy()
+hourly_soil_moisture_0_to_7cm = hourly.Variables(26).ValuesAsNumpy()
+hourly_soil_moisture_7_to_28cm = hourly.Variables(27).ValuesAsNumpy()
+hourly_soil_moisture_28_to_100cm = hourly.Variables(28).ValuesAsNumpy()
+hourly_soil_moisture_100_to_255cm = hourly.Variables(29).ValuesAsNumpy()
+
+# Adjust the date range to match only the hours of "yesterday"
+yesterday_start = pd.to_datetime(f"{start_date} 00:00:00", utc=True)
+yesterday_end = pd.to_datetime(f"{end_date} 23:59:59", utc=True)
+
+hourly_data = {
+    "date": pd.date_range(
+        start=yesterday_start,
+        end=yesterday_end,
+        freq='H',  # Set frequency to hourly
+        inclusive="left"
+)}
+hourly_data["temperature_2m"] = hourly_temperature_2m
+hourly_data["relative_humidity_2m"] = hourly_relative_humidity_2m
+hourly_data["dew_point_2m"] = hourly_dew_point_2m
+hourly_data["apparent_temperature"] = hourly_apparent_temperature
+hourly_data["precipitation"] = hourly_precipitation
+hourly_data["rain"] = hourly_rain
+hourly_data["snowfall"] = hourly_snowfall
+hourly_data["snow_depth"] = hourly_snow_depth
+hourly_data["weather_code"] = hourly_weather_code
+hourly_data["pressure_msl"] = hourly_pressure_msl
+hourly_data["surface_pressure"] = hourly_surface_pressure
+hourly_data["cloud_cover"] = hourly_cloud_cover
+hourly_data["cloud_cover_low"] = hourly_cloud_cover_low
+hourly_data["cloud_cover_mid"] = hourly_cloud_cover_mid
+hourly_data["cloud_cover_high"] = hourly_cloud_cover_high
+hourly_data["et0_fao_evapotranspiration"] = hourly_et0_fao_evapotranspiration
+hourly_data["vapour_pressure_deficit"] = hourly_vapour_pressure_deficit
+hourly_data["wind_speed_10m"] = hourly_wind_speed_10m
+hourly_data["wind_speed_100m"] = hourly_wind_speed_100m
+hourly_data["wind_direction_10m"] = hourly_wind_direction_10m
+hourly_data["wind_direction_100m"] = hourly_wind_direction_100m
+hourly_data["wind_gusts_10m"] = hourly_wind_gusts_10m
+hourly_data["soil_temperature_0_to_7cm"] = hourly_soil_temperature_0_to_7cm
+hourly_data["soil_temperature_7_to_28cm"] = hourly_soil_temperature_7_to_28cm
+hourly_data["soil_temperature_28_to_100cm"] = hourly_soil_temperature_28_to_100cm
+hourly_data["soil_temperature_100_to_255cm"] = hourly_soil_temperature_100_to_255cm
+hourly_data["soil_moisture_0_to_7cm"] = hourly_soil_moisture_0_to_7cm
+hourly_data["soil_moisture_7_to_28cm"] = hourly_soil_moisture_7_to_28cm
+hourly_data["soil_moisture_28_to_100cm"] = hourly_soil_moisture_28_to_100cm
+hourly_data["soil_moisture_100_to_255cm"] = hourly_soil_moisture_100_to_255cm
+
+
+# Process the data and save it to a CSV file
+daily_dataframe = pd.DataFrame(data=hourly_data)
+print(daily_dataframe)
+csv_file = f"weather_data_{two_day_ago.strftime('%Y_%m_%d')}.csv"
+daily_dataframe.to_csv(csv_file, header=True)
+print(f"Weather data has been saved to {csv_file}")
