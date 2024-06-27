02710ca2df6266e12583998c692fe230adede1f6
Florent-COMPAGNONI <florent.compagnoni+dev@gmail.com>
2024-06-27 18:39:23 +0200

    change to GET

--- app.py
+++ app.py
@@ -15,38 +15,38 @@ scaler_target = joblib.load("./static/scalers/scaler_target_v1.pkl")
 app = FastAPI()
 
 
-class WeatherParams(BaseModel):
-    date: datetime.datetime
-    temperature_2m: float
-    relative_humidity_2m: float
-    dew_point_2m: float
-    apparent_temperature: float
-    precipitation: float
-    rain: float
-    snowfall: float
-    snow_depth: float
-    weather_code: int
-    pressure_msl: float
-    surface_pressure: float
-    cloud_cover: float
-    cloud_cover_low: float
-    cloud_cover_mid: float
-    cloud_cover_high: float
-    et0_fao_evapotranspiration: float
-    vapour_pressure_deficit: float
-    wind_speed_10m: float
-    wind_speed_100m: float
-    wind_direction_10m: float
-    wind_direction_100m: float
-    wind_gusts_10m: float
-    soil_temperature_0_to_7cm: float
-    soil_temperature_7_to_28cm: float
-    soil_temperature_28_to_100cm: float
-    soil_temperature_100_to_255cm: float
-    soil_moisture_0_to_7cm: float
-    soil_moisture_7_to_28cm: float
-    soil_moisture_28_to_100cm: float
-    soil_moisture_100_to_255cm: float
+# class WeatherParams(BaseModel):
+#     date: datetime.datetime
+#     temperature_2m: float
+#     relative_humidity_2m: float
+#     dew_point_2m: float
+#     apparent_temperature: float
+#     precipitation: float
+#     rain: float
+#     snowfall: float
+#     snow_depth: float
+#     weather_code: int
+#     pressure_msl: float
+#     surface_pressure: float
+#     cloud_cover: float
+#     cloud_cover_low: float
+#     cloud_cover_mid: float
+#     cloud_cover_high: float
+#     et0_fao_evapotranspiration: float
+#     vapour_pressure_deficit: float
+#     wind_speed_10m: float
+#     wind_speed_100m: float
+#     wind_direction_10m: float
+#     wind_direction_100m: float
+#     wind_gusts_10m: float
+#     soil_temperature_0_to_7cm: float
+#     soil_temperature_7_to_28cm: float
+#     soil_temperature_28_to_100cm: float
+#     soil_temperature_100_to_255cm: float
+#     soil_moisture_0_to_7cm: float
+#     soil_moisture_7_to_28cm: float
+#     soil_moisture_28_to_100cm: float
+#     soil_moisture_100_to_255cm: float
 
 
 # Helper function to prepare data for prediction
@@ -58,8 +58,8 @@ def prepare_data(features, scaler_features, time_step=24):
     return np.array(X)
 
 
-@app.post("/predict/")
-async def predict(request: list[WeatherParams]):
+@app.get("/predict/")
+async def predict():
     file_path = "weather_data_2024_06_25.csv"
 
     # Read the CSV file
